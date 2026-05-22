c06859b709f8ae00e53775a0fff195880ddf2aea
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-22 18:56:45 -0300

    Agregar users.controller

--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -3,40 +3,29 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('users') // Esto significa que todas las rutas de este archivo van a empezar con /users
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post() // Escucha peticiones HTTP POST a '/users' (para registrar un trabajador)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+  @Get() // Escucha peticiones HTTP GET a '/users' (para listar todos los trabajadores)
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Get(':id') // Escucha peticiones HTTP GET a '/users/:id' (para buscar un trabajador por su ID)
+  // Usamos ParseUUIDPipe para asegurarnos de que el ID que nos pasan sea un UUID válido antes de buscarlo
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.findOne(id);
   }
 }
