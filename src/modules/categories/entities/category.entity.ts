1d38e2bea62302068cbec530397549063a6d184e
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-23 14:54:02 -0300

    Agregar Entity de categories

--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,25 @@
-export class Category {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'categories' }) // Nombre de la tabla en PostgreSQL
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', unique: true, length: 100 })
+  name: string;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
