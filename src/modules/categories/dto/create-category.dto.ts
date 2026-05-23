0fd7fe507e2ef96196b5fe9e3f4483697da507e3
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-23 15:07:46 -0300

    Agregar class-validator en create-category.dto

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString({ message: 'El nombre de la categoría debe ser un texto' })
+  @IsNotEmpty({ message: 'El nombre de la categoría no puede estar vacío' })
+  @MaxLength(100, { message: 'El nombre no puede superar los 100 caracteres' })
+  name: string;
+}
