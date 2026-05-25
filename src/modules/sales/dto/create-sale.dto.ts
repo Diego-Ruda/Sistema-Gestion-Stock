d9ee671a83db8c3b6a9bc8b9a6cb09afe5bcaf0a
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-25 19:27:15 -0300

    Agregar el DTO para sales

--- src/modules/sales/dto/create-sale.dto.ts
+++ src/modules/sales/dto/create-sale.dto.ts
@@ -1,1 +1,15 @@
-export class CreateSaleDto {}
+import { IsNotEmpty, IsInt, IsPositive, IsUUID } from 'class-validator';
+
+export class CreateSaleDto {
+  @IsUUID('4', { message: 'El productId debe ser un UUID válido' })
+  @IsNotEmpty({ message: 'El ID del producto es obligatorio para la venta' })
+  productId: string;
+
+  @IsUUID('4', { message: 'El employeeId debe ser un UUID válido' })
+  @IsNotEmpty({ message: 'El ID del empleado es obligatorio para la venta' })
+  employeeId: string;
+
+  @IsInt({ message: 'La cantidad debe ser un número entero' })
+  @IsPositive({ message: 'La cantidad a vender debe ser mayor a 0' })
+  quantity: number;
+}
