7784fae6bd3519ad3e21f9469e05ed0c0f387eaf
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-25 20:28:52 -0300

    Agregar entity de sales

--- src/modules/sales/entities/sale.entity.ts
+++ src/modules/sales/entities/sale.entity.ts
@@ -1,1 +1,30 @@
-export class Sale {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+import { User } from '../../../auth/users/entities/user.entity';
+
+@Entity({ name: 'sales' })
+export class Sale {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'int' })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total: number;
+
+  @ManyToOne(() => Product, (product) => product.id, { eager: true })
+  product: Product;
+
+  @ManyToOne(() => User, (user) => user.id, { eager: true })
+  employee: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+}
