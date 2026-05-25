270ac0e2afea01067f41d38eed5642a14adfdb6e
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-25 14:52:06 -0300

    Agregar Entity de product

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,1 +1,36 @@
-export class Product {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Category } from '../../categories/entities/category.entity';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', unique: true, length: 150 })
+  name: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  price: number;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @ManyToOne(() => Category, (category) => category.id, { eager: true })
+  category: Category;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
