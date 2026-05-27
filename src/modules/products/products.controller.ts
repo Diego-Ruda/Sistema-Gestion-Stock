a5ba5662dedd5d82f296ddb6afb0e0908b42b8ee
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-27 19:21:30 -0300

    Agregar Actualizacion en el controller

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -38,5 +39,13 @@ export class ProductsController {
   @Patch(':id/activate')
   activate(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.activate(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 }
